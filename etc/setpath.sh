91dff04bfd05470b3426e759afd47cf033579e4e
DrJeffLee <jeffrey.johnston@qub.ac.uk>
2025-09-23 18:05:37 +0100

    Added script for setting environment path (#5)
    * files moved and housecleaning
    
    * changed file permissions
    
    * new script for setting path

---
+++ etc/setpath.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e # Exit on error
+
+# Set root directory
+SWAN_DIR="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+SWAN_DIR="$(readlink -f "$SWAN_DIR")"
+
+echo "Settting paths..."
+
+# Directories to add
+BIN_DIR="$SWAN_DIR/bin"
+LIB_DIR="$SWAN_DIR/lib"
+
+# Export paths
+export PATH="$BIN_DIR:$PATH"
+export LD_LIBRARY_PATH="$LIB_DIR:$LD_LIBRARY_PATH"
+
+echo "Environment updated:"
+echo "  SWAN_DIR=$SWAN_DIR"
+echo "  BIN_DIR=$BIN_DIR"
+echo "  LIB_DIR=$LIB_DIR"
