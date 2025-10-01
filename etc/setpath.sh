5d0937cdfd86d4ee8f06721646174c4353b7819d
Jeffrey Lee Johnston <jeffrey.johnston@qub.ac.uk>
2025-10-02 00:38:28 +0100

    modified PATH update

--- etc/setpath.sh
+++ etc/setpath.sh
@@ -9,13 +9,13 @@ echo "Settting paths..."
 
 # Directories to add
 BIN_DIR="$SWAN_DIR/bin"
-LIB_DIR="$SWAN_DIR/lib"
+#LIB_DIR="$SWAN_DIR/lib"
 
 # Export paths
-export PATH="$BIN_DIR:$PATH"
-export LD_LIBRARY_PATH="$LIB_DIR:$LD_LIBRARY_PATH"
+export PATH="$PATH:$BIN_DIR"
+#export LD_LIBRARY_PATH="$LIB_DIR:$LD_LIBRARY_PATH"
 
 echo "Environment updated:"
-echo "  SWAN_DIR=$SWAN_DIR"
+#echo "  SWAN_DIR=$SWAN_DIR"
 echo "  BIN_DIR=$BIN_DIR"
-echo "  LIB_DIR=$LIB_DIR"
+#echo "  LIB_DIR=$LIB_DIR"
